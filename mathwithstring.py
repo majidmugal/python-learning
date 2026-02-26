fb5990f7e06d3bdc5cd71a37e17dadfd872987d0
majidmugal <majidmugal7737713356@gmail.com>
2026-02-26 13:04:53 +0530

    math_with_string

---
+++ mathwithstring.py
@@ -0,0 +1,50 @@
+#a = input("hello world")
+#print(a)
+
+#print(input("enter a number:"))
+
+##operators
+# +, -, /, //, *, **
+
+#airthmatic with int ->
+
+#print( 2 + 3)
+#print( 5 - 6)
+#print( 3/2)
+#print( 10 / 3)
+#print ( 10 // 3)
+#print ( 10 % 3)
+#print ( 3 * 5)
+#print( 3 ** 2)
+
+#Airtmatic with float
+
+#print ( 2.5  + 2.5)
+
+
+#both int and float
+
+#print(3.4 + 3)
+
+#string
+
+#print ("hello" + "world")
+
+#a = "hello" 
+#b = "world"
+#print (a + b)
+
+#a = input("enter a number:")
+#b = input("enter a number:")
+
+#a = int(a)
+#b = int(b)
+
+#print(a + b)
+
+#a = int( input ("enter a number"))
+#b = int(input ("enter a number"))
+
+#print( a * b)
+
+
