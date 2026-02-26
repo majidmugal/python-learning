aeee1b9fd3ffd14ee2a033dcadf2a4621473354d
majidmugal <majidmugal7737713356@gmail.com>
2026-02-26 13:06:01 +0530

    operato-python

---
+++ operator.py
@@ -0,0 +1,125 @@
+#OPERATOR
+
+#ASSIGNEMENT/SHORTEND
+#+=, -=, *=, /=, %=, //=, **=
+
+#a = 6
+
+#a **= 3
+
+#print(a)
+
+#comparison operator
+# >, <, ==, >=, <=, !=
+
+#a = 50
+#b = 50
+
+#print(a <= b)
+
+
+
+ #LOGICAL-OPERATOR
+
+# AND , OR . NOT
+
+
+#And operator
+
+
+# number =  34
+
+# print(number > 10 or number < 20)
+
+
+# membership operator
+
+# a = "i live in india"
+# print("india" in )
+
+
+# username = input("enter your username: ")
+# if username == "admin":
+#     print("you can add other")
+# else:
+#     print("you can't add")]
+
+
+
+
+
+
+
+# print vowel and consonant
+
+
+# char = input("enter a single char: ")
+
+# if char == "a":
+#     print("this is a vowel")
+# elif char == "e":
+#     print("this is a vowel")
+# elif char == "i":
+#     print("this is a vowel")
+# elif char == "o":
+#     print("this is a vowel")
+# elif char == "u":
+#     print("this is a vowel")
+# else:
+#     print("this is consonant")
+
+
+
+# char = input("enter a single char: ")
+
+# if char in "aeiouAEIOU":
+#     print("this is a vowel")
+# else:
+#     print("this is consonant")
+
+
+
+
+
+
+# match case 
+
+# char = input("enter only one chracter: ")
+
+# match char:
+
+#     case "a":
+#         print("vowel")
+#     case "e":
+#         print("vowel")
+#     case "i":
+#         print("vowel")
+#     case "o":
+#         print("vowel")
+#     case "u":
+#         print("vowel")
+#     case _:
+#         print("consonant")
+
+
+
+
+# number = int(input("enter a number: "))
+
+# match number :
+#     case 1:
+#         print("sunday")
+#     case 2:
+#         print("monday")
+#     case 3:
+#         print("tuesday")
+#     case 4:
+#         print("wednesday")
+#     case 5:
+#         print("thursady")
+#     case 6:
+#         print("friday")
+#     case 7:
+#         print("saturday")
+#     case _:
+#         print("invalid number")
