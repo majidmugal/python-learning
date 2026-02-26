e2ea6f635bc71849f6fcaf30c8d6496543b16dd5
majidmugal <majidmugal7737713356@gmail.com>
2026-02-26 12:59:53 +0530

    python_if-else

---
+++ if-else.py
@@ -0,0 +1,15 @@
+# number = int(input("enter a number: "))
+
+# if number < 0 :
+#     print("number is positive.")
+# else : 
+#     print("number i negative")
+
+number = int(input("enter a number: "))
+
+if number > 0 :
+    print("number is positive: ")
+elif number < 0 :
+    print("number is negative: ")
+else:
+    print("number is 0: ")
