aa090a9a10065ef60a32fffd432e4bf60c22f1b8
majidmugal <majidmugal7737713356@gmail.com>
2026-02-26 13:06:38 +0530

    type-conversion

---
+++ typeconversion.py
@@ -0,0 +1,43 @@
+
+##Flot -> intiger
+ 
+ 
+#a = 45.6
+
+#print( int(a))
+#print(a)
+
+
+ 
+##int->float
+
+#a = 25
+#a = float(a)
+#print (a)
+
+
+#int->str
+
+ 
+#a = 20
+#a = str(a)
+#print(type](a))
+
+#a = 35
+#print(type(str(a))) 
+
+
+##flot -> str
+
+#a = 34.2
+#print(type(str(a)))
+
+
+## str -> int
+a = "34"
+print(type(int(a)))
+
+
+
+
+ 
