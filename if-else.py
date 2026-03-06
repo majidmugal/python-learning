2952ad823ffa1a0e24b518870da9662119180b00
majidmugal <majidmugal7737713356@gmail.com>
2026-03-06 12:02:12 +0530

    if-else

--- if-else.py
+++ if-else.py
@@ -1,15 +1,185 @@
-# number = int(input("enter a number: "))
+# age = 16
 
-# if number < 0 :
-#     print("number is positive.")
-# else : 
-#     print("number i negative")
+# if age >= 18 :
+#     print("Yes You Can Vote.")
+# else:
+#     print("No You Can Not Vote.")
 
-number = int(input("enter a number: "))
 
-if number > 0 :
-    print("number is positive: ")
-elif number < 0 :
-    print("number is negative: ")
-else:
-    print("number is 0: ")
+# number = 0
+
+# if number > 0:
+#     print("It's +ve")
+
+# elif number < 0 :
+#     print("It's -ve")
+
+# else:
+#     print("It's 0.")
+
+
+# a = "u"
+
+# if a == "a":
+#     print("yes")
+# elif a == "e":
+#     print("yes")
+# elif a == "i":
+#     print("yes")  
+# elif a == "o":
+#     print("yes")
+# elif a == "u":
+#     print("yes")
+# else:
+#     print("No")
+
+
+
+# a = "A"
+
+# if a in "aeiouAEIOU":
+#     print("Yes")
+# else:
+#     print("No")
+
+
+# a = 9
+
+# if a < 50 and a > 10:
+#     print("yes")
+# else:
+#     print("no")
+
+
+# char = 'v'
+
+# if char in "aeiouAEIOU":
+#     print("Yes")
+# else:
+#     print("Not")
+
+# print("Start")
+
+# if 4 > 5:
+#     print("I am in the if statement")
+#     print("I am in the if statement")
+# else:
+#     print("I am in the else statement")
+#     print("I am in the else statement")
+
+# print("end")
+
+
+
+
+# number = -34123412431234817264387164327
+
+# if number > 0 :
+#     print("It's a +ve Number")
+# elif number < 0:
+#     print("It's a -ve Number")
+# else:
+#     print("It's a 0")
+
+
+
+
+
+# Even or Odd Number
+# Write a program that takes an integer input from the user and prints whether the number is even or odd.
+
+# number = int( input("Enter A number: ") )
+
+
+# if number % 2 == 0:
+#     print("It's and Even Number")
+# else:
+#     print("It's an Odd number.")
+
+
+
+# Positive, Negative, or Zero
+# Ask the user to enter a number and determine whether the number is positive, negative, or zero.
+
+# Age Category Checker
+# Take the user's age as input and print a message indicating if they are a child, teenager, adult, or senior.
+
+# 10,
+# 18,
+# 35,
+# 35>
+
+# age = int(input("Enter Your Age: "))
+
+# if age <= 10:
+#     print("Person is a Child.")
+
+# elif age > 10 and age <= 18 :
+#     print("Person is a Teenager.")
+
+# elif age > 18 and age <= 35 :
+#     print("Person is a Adult.")
+
+# else:
+#     print("Person is a Senior.")
+
+
+# Grade Calculator
+# Write a program that takes a student's score (0–100) and prints the corresponding letter grade (A, B, C, D, or F).
+
+# score = int( input("Enter Your Score: ") )
+
+
+
+# Vowel or Consonant Checker
+# Ask the user to input a single letter and determine if it's a vowel, consonant, or an invalid character.
+
+# Simple Login System
+# Simulate a login system where the user enters a username and password. Print a success message if both are correct, otherwise print an error.
+
+
+# Number Comparison
+# Take two numbers as input from the user and compare them. Print whether the first is greater, the second is greater, or if they are equal.
+
+# number1 = int(input("Enter 1st Number: "))
+# number2 = int(input("Enter 2nd Number: "))
+# number3 = int(input("Enter 2nd Number: "))
+
+
+# if number1 > number2 and number1 > number3:
+#     print( f"{number1} Is Greater." )
+# elif number2 > number1 and number2 > number3:
+#     print( f"{number2} Is Greater." )
+# else:
+#     print( f"{number3} Is Greater." )
+
+
+
+# match month:
+
+#     case 1:
+#         print("31 Days")
+#     case 2:
+#         print("29 Days")
+#     case 3:
+#         print("31 Days")
+#     case 4:
+#         print("30 Days")
+#     case 5:
+#         print("31 Days")
+#     case 6:
+#         print("30 Days")
+#     case 7:
+#         print("31 Days")
+#     case 8:
+#         print("31 Days")
+#     case 9:
+#         print("30 Days")
+#     case 10:
+#         print("31 Days")
+#     case 11:
+#         print("30 Days")
+#     case 12:
+#         print("31 Days")
+#     case _:
+#         print("Invalid Month")
