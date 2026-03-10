cacec70ea42d5e03ab380ce434a65f3a94e11de8
majidmugal <majidmugal7737713356@gmail.com>
2026-03-10 15:33:07 +0530

    list-prectics

---
+++ list_prec.py
@@ -0,0 +1,108 @@
+# a = [0,1,2,3,4,5,6,7,8,9]
+
+# a[0] = 76
+
+# print(a)
+
+# abc = [ "Vipin", 25, "Jaipur", 302020, True, [ "Test", 43, [23242, [ 234234]] ] ]
+
+# print(abc[5][2][0])
+
+
+
+# refrence varible 
+
+# a = ["sdkw", 2 , 3 , 6]
+
+# b = a
+
+# b[0] = "majid"
+
+# print(b)
+
+
+
+
+# numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# # print(f"slice [2:7] : {numbers [2:7]}")
+
+# print(f"slice [5::] : {numbers [5:]}")
+
+
+
+
+
+# my_list = ["apple" , "banana" ,"cherry" , "date"]
+
+# # my_list[2] = "mango"
+
+# # print(f"modified single element {my_list}")
+
+# my_list[0:2] = "majid" ,"mugal"
+
+# print(my_list)
+
+
+
+
+#concatination list
+
+# list1 = [1,2]
+# list2= [3,4]
+
+# c = list1+list2
+
+# print(c)
+
+
+
+#Membership list
+
+# list1 = [1,2,3,4,5]
+
+# print(5 in list1)
+
+
+
+#my_list = ["apple", "grape", "cherry", "banana"]
+
+# my_list.append("majid")
+
+# print(my_list)
+
+
+
+# my_list.insert(-1, "majid")
+
+# print(my_list)
+
+
+# my_list.remove("cherry")
+
+# print(my_list)
+
+
+#my_list.pop(2)
+
+#print(my_list)
+
+
+
+
+# numbers = [5, 2, 8, 1, 9, 11]
+
+# # # Using .sort() (in-place)
+# numbers.sort(reverse = True)
+
+# print(numbers) 
+# # [11, 9, 8, 5, 2, 1]
+
+
+words = ["majida", "majidb", "majidd", "majidc", "majidf"]
+
+words.sort(reverse = True)
+
+print(words)
+
+
