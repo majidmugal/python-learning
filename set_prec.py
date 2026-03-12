b385236a5930c408d60713981aa7973e47ee03c5
majidmugal <majidmugal7737713356@gmail.com>
2026-03-12 12:32:10 +0530

    set prectics

---
+++ set_prec.py
@@ -0,0 +1,120 @@
+# create empty set
+
+# empty_set = set()
+# print(type(empty_set))
+
+
+# a = { 1, 2, 3, 4, 5, 6, 1, 2, "mazid", "Mazid" }
+# print(a)
+
+
+
+# fruits = {"apple", "banana", "cherry", "Banana", 1, 3, 4, 5, 1}
+# print(f"set of fruits: {fruits}")
+
+
+# sorted_data = sorted("98765432" ,reverse = True)
+# print(sorted_data)
+
+
+
+# mixed_set = {1, "hello", (1, 2), 3.14, (1, 2)}
+# print(f"mixed set: {mixed_set}")
+
+
+# my_set = {1, 2, 3}
+
+
+# my_set.add(4)
+# print(my_set)
+
+
+# my_set.update([1,2,3,4,5,6])
+# print(my_set)
+
+
+# my_set.update("abcdefgh")
+# print(my_set)
+
+
+
+# new_set = {10,20,30,40}
+
+# new_set.remove(30)
+# print(new_set)
+
+
+# new_set.clear()
+# print(new_set)
+
+
+
+# a = {1,2,3}
+# b = a.copy()
+# b.add(23)
+
+# print(a)
+
+
+
+#membership
+# my_set = {1,2,3,4,5} 
+# print(is 2 in my_set)
+# print(6 not in my_set)
+
+
+# set_a = {1,2,3}
+# set_b = {3,4,5}
+
+# union_set = set_a | set_b
+
+# # print(union_set)
+# print(set_a.union(set_b))
+
+
+
+#symmetric diffrence
+
+# a = {1,2,3}
+# b = {3,4,5}
+
+# print(a.symmetric_difference(b))
+
+# print(a.intersection(b))
+
+
+
+set1 = {1, 2, 3}
+set2 = {1, 2, 3, 4, 5}
+set3 = {4, 5, 6}
+
+
+#print(set1.issubset(set2))
+
+# print(set2.issuperset(set1))
+
+# print(set1.isdisjoint(set1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
