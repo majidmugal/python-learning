cafeda874c45ffa3a6661ff895703fead1b279c7
majidmugal <majidmugal7737713356@gmail.com>
2026-03-31 14:20:41 +0530

    classes in python

---
+++ class.py
@@ -0,0 +1,59 @@
+# class socilamedia():
+
+
+#     CEO = "mark"            #varible is attribue in class.  
+
+#     def like(self):
+#        print("liked")
+#     def comment(self):        # this is mthoad is in class and his work in body of class
+#         print("cmnnt")
+
+# facebook = socilamedia()
+
+# # print(facebook.CEO.upper)
+
+# # facebook.comment()
+
+# twitter = socilamedia()
+
+# twitter.CEO = "elon"
+
+# print(facebook.CEO)
+# print(twitter.CEO)
+
+
+
+
+
+
+
+class car():
+
+    color = "black"
+    brand = "rolls royce"
+    milage = "3mph"
+
+    def engine(a):
+        print("rr engine")
+
+    def drive(b):
+        print("driving")
+
+    def horn(c):
+        print("pe_pe_pe_pe")
+
+a = car()
+b = car()
+
+b.brand = "BMW"
+b.color = "blue"
+
+# print(a.brand)
+# print(b.brand)
+
+
+print(b.color)
+print(a.color)
+
+    
+    
